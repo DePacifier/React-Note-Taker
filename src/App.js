ea138f11c297c7e0d5e6c21559590f216e028ed7
DePacifier <milkybekele@gmail.com>
2022-03-18 13:41:40 +0300

    completed adding and linking the main pages to work on for the app and installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+// Page Imports
+import NotesPage from "./pages/Notes.page";
+import CreatePage from "./pages/Create.page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<NotesPage />} />
+        <Route path="create" element={<CreatePage />} />
+      </Routes>
+    </main>
   );
 }
 
